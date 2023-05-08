ce752efacead23115471bffe53cdaafca3120efd
Twayfrancis <tawaifrancis@gmail.com>
2023-05-08 12:06:01 -0700

    11. Hello, write

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import sys
+# use the write function from sys module to print
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+# exit with a status code of 1
+sys.exit(1)
