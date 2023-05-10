f4ba0f061eadb3f4651fac6de53ee80273b50743
Twayfrancis <tawaifrancis@gmail.com>
2023-05-10 12:01:43 -0700

    13. Insert in sorted linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,54 +1,42 @@
-#include "lists.h"
 #include <stdlib.h>
+#include "lists.h"
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
+ * insert_node - Inserts a number into a sorted singly linked list.
+ * @head: Pointer to the head of the linked list.
+ * @number: Number to be inserted.
  *
- * Description: singly linked list node structure
- * for Holberton project
- */
-typedef struct listint_s
-{
-    int n;
-    struct listint_s *next;
-} listint_t;
-
-/**
- * insert_node - inserts a number into a sorted singly linked list
- * @head: pointer to a pointer to the head of the list
- * @number: the number to insert
- *
- * Return: the address of the new node, or NULL if it failed
+ * Return: Address of the new node, or NULL if it failed.
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new_node, *current;
+	listint_t *new_node, *current;
+	/*check if the head pointer is NULL*/
+	if (head == NULL)
+		return (NULL);
+	/*allocate memory for the new node*/
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	/* assign the number to the new node*/
+	new_node->n = number;
+	new_node->next = NULL;
 
-    if (!head)
-        return (NULL);
+	/*if the list is empty or num is smaller thatn current head value*/
+	if (*head == NULL || number < (*head)->n)
+	{
+		/* insert the new node at the beginning of the list*/
+		new_node->next = *head;
+		*head = new_node;
+	return (new_node);
+	}
+	/*find the appropriate position to the insert the new node*/
+	current = *head;
+	while (current->next != NULL && number > current->next->n)
+		current = current->next;
+	/*insert the new node into the list*/
+	new_node->next = current->next;
+	current->next = new_node;
 
-    new_node = malloc(sizeof(listint_t));
-    if (!new_node)
-        return (NULL);
-
-    new_node->n = number;
-    new_node->next = NULL;
-
-    if (!*head)
-    {
-        *head = new_node;
-        return (new_node);
-    }
-
-    current = *head;
-
-    while (current->next && current->next->n < number)
-        current = current->next;
-
-    new_node->next = current->next;
-    current->next = new_node;
-
-    return (new_node);
+	return (new_node);
 }
