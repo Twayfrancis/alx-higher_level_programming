923b77f4a01f8fe6f7225ebc4994605f5e9c7a0d
Twayfrancis <tawaifrancis@gmail.com>
2023-05-14 13:09:43 -0700

    8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@ def multiple_returns(sentence):
     if not sentence:
         raise ValueError("sentence cannot be empty")
     length = len(sentence)
-    first = sentence[0] if sentence else None
-    return (length, first)
+    first_character = sentence[0] if length > 0 else None
+    return (length, first_character)
