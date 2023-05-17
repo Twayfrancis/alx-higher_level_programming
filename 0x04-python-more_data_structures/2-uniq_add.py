8aaf3dc95cfbe2bcd6c507d48137d07dcd934db6
Twayfrancis <tawaifrancis@gmail.com>
2023-05-17 12:15:11 -0700

    2. Unique addition

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_list = []
+    for item in my_list:
+        if item not in unique_list:
+            unique_list.append(item)
+    sum = 0
+    for item in unique_list:
+        sum += item
+    return sum
