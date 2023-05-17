626e7aa3bd8bb9e49c25602283f2d645ce65a025
Twayfrancis <tawaifrancis@gmail.com>
2023-05-17 14:13:25 -0700

    13. Weighted average!

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    sum_of_weights = sum(weight for score, weight in my_list)
+    sum_of_scores = sum(score * weight for score, weight in my_list)
+    return sum_of_scores / sum_of_weights
