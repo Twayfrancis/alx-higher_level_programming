f6a854ecd9462f06184959de52712789de858e4f
Twayfrancis <tawaifrancis@gmail.com>
2023-05-17 02:20:27 -0700

    fix correct ouput

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,18 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if not isinstance(matrix, list):
-        return None
-    if len(matrix) == 0:
-        return []
-    if not isinstance(matrix[0], list):
-        return None
-    if len(matrix[0]) == 0:
-        return []
-    new_matrix = []
-    for row in matrix:
-        new_row = []
-        for value in row:
-            new_row.append(value ** 2)
-        new_matrix.append(new_row)
-
-    return new_matrix
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
