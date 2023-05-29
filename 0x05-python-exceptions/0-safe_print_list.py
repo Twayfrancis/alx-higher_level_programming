9499c4be1b951fd337ee19311982e3e8be2a62ef
Twayfrancis <tawaifrancis@gmail.com>
2023-05-29 11:56:32 -0700

    0. Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    tway  = 0
     try:
         for i in range(x):
             print(my_list[i], end="")
-        print()
-        return x
+            tway += 1
     except IndexError:
-        print()
-        return len(my_list)
+        pass
+    print()
+    return tway
