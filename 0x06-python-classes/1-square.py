07e0f8bf593ee69460acd6866e076d61e8779b91
Twayfrancis <tawaifrancis@gmail.com>
2023-05-30 12:24:10 -0700

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""This clss defines a square."""
+
+
+class Square:
+
+    __size = None
+
+    def __init__(self, size):
+
+        """Initializes the square with the given size."""
+
+        self.__size = size
