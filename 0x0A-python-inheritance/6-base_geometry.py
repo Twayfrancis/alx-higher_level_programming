64c0135d5e1188d4340fe53be3ca65557af55efe
Twayfrancis <tawaifrancis@gmail.com>
2023-06-12 13:10:06 -0700

    a class BaseGeometry (based on 5-base_geometry.py).
    Public instance method: def area(self): that raises an Exception with the message area() is not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class BaseGeometry (based on 5-base_geometry.py)."""
+
+
+class BaseGeometry:
+    """represents base-geometry"""
+    def area(self):
+        """Compute the area of the geometry."""
+        raise Exception("area() is not implemented")
