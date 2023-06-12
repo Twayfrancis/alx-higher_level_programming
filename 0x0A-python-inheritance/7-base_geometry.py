f265d9cdf6ec70860d4813402798e22a3737ba94
Twayfrancis <tawaifrancis@gmail.com>
2023-06-12 13:28:55 -0700

    fixing E301 expected 1 blank line, found 0

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -4,14 +4,17 @@
 
 class BaseGeometry:
     """class with public instance mtds"""
+
     def area(self):
         """that raises an Exception with the message
         area() is not implemented
         """
+
         raise Exception('area() is not implemented')
+
     def integer_validator(self, name, value):
-         """ that validates value"""
-         if type(value) is not int:
-             raise TypeError("{} must be an integer".format(name))
-         if value <= 0:
-             raise ValueError("{} must be greater than 0".format(name))
+        """ that validates value"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
