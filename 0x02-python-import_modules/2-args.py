cf24cebb0117f514e66af211b2b1bfbee768b40d
Twayfrancis <tawaifrancis@gmail.com>
2023-06-23 12:10:37 -0700

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == '__main__':
     num = len(sys.argv) - 1
     arg_list = sys.argv[1:]
-    print("{} argument{}:".format(num, 's' if num != 1 else ''), end='')
+    print("{} argument{}".format(num, 's' if num != 1 else ''), end='')
 
     if num == 0:
         print(".")
