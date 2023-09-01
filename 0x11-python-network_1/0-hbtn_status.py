87b8065190ee3af361eb361ab62568364a6ce422
Twayfrancis <tawaifrancis@gmail.com>
2023-09-01 13:29:06 -0700

    Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+from urllib import request
+
+
+if __name__== "__main__":
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        body = response.read()
+
+        print('Body response:')
+        print('- type:', type(body))
+        print('- content:', repr(body))
+        print('- utf8 content:', body.decode('utf-8'))
