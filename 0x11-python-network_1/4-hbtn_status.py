175a87412ab0e68355c020ad32405efc42c1c70e
Twayfrancis <tawaifrancis@gmail.com>
2023-09-01 15:16:13 -0700

    fix correct output

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,5 @@ import requests
 if __name__ == '__main__':
     response = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
-    print('\t- type:', type(response.content))
-    print('\t- content:', response.content)
+    print('\t- type:', type(response.text))
+    print('\t- content:', response.text)
