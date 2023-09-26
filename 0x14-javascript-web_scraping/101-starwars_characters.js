552329acc5c5d60a9574b47223f65f46444b264c
root <tawaifrancis@gmail.com>
2023-09-26 14:59:10 -0700

    fix semistandard

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -7,21 +7,21 @@ const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request(apiUrl, (error, response, body) => {
-	if (!error && response.statusCode === 200) {
-		const filmData = JSON.parse(body);
-		const characters = filmData.characters;
+  if (!error && response.statusCode === 200) {
+    const filmData = JSON.parse(body);
+    const characters = filmData.characters;
 
-		characters.forEach(characterUrl => {
-			request(characterUrl, (error, response, body) => {
-				if (!error && response.statusCode === 200) {
-					const characterData = JSON.parse(body);
-					console.log(characterData.name);
-				} else {
-					console.error(`Error fetching character data: ${error}`);
-				}
-			});
-		});
-	} else {
-		console.error(`Error fetching film data: ${error}`);
-	}
+    characters.forEach(characterUrl => {
+      request(characterUrl, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          const characterData = JSON.parse(body);
+          console.log(characterData.name);
+        } else {
+          console.error(`Error fetching character data: ${error}`);
+        }
+      });
+    });
+  } else {
+    console.error(`Error fetching film data: ${error}`);
+  }
 });
