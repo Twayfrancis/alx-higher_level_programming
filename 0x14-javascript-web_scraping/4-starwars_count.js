49966eed6a6fa3946ff31d6dbbe6eee86b434cb4
root <tawaifrancis@gmail.com>
2023-09-26 14:02:08 -0700

    fix correct out put

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,20 +2,20 @@
 
 const request = require('request');
 
-const apiUrl = process.argv[2];
-const wedgeAntillesId = 18;
-
-const url = `${apiUrl}?characters=${wedgeAntillesId}`;
-
+const url = process.argv[2];
 request(url, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    const movies = JSON.parse(body).results;
-    const wedgeAntillesMovies = movies.filter((movie) => {
-      return movie.characters.includes(wedgeAntillesId);
-    });
-
-    console.log(wedgeAntillesMovies.length);
+    let counter = 0;
+    const results = JSON.parse(body).results;
+    for (let i = 0; i < results.length; i++) {
+      for (let j = 0; j < results[i].characters.length; j++) {
+        if (results[i].characters[j].slice(-3, -1) === '18') {
+          counter++;
+        }
+      }
+    }
+    console.log(counter);
   }
 });
