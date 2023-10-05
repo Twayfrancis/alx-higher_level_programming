e5f91aaacdf560f0a8ab61b976f461c655971327
unknown <tawaifrancis@gmail.com>
2023-10-05 22:39:17 +0300

    JavaScript script that fetches and lists the title for all movies by using this URL: https://swapi-api.alx-tools.com/api/films/?format=json~

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,9 @@
+$.ajax({
+    url: "https://swapi-api.alx-tools.com/api/films/?format=json",
+    method: "GET",
+    success: function(data) {
+        data.results.forEach(function(movie) {
+            $("UL#list_movies").append("<li>" + movie.title + "</li>");
+        });
+    }
+});
